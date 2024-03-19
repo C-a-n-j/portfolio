83c226707bf3a803274b0850171b0243bd4275d5
c-a-n-j <eddymacharia@gmail.com>
2024-03-19 13:13:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Web-developer",
+    "frontend Developer",
+    "Backend Developer",
+    "youtuber",
+  ],
+  typespeed: 150,
+  backspeed: 100,
+
+  loop: true,
+});
